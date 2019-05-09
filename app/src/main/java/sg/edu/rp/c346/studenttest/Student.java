020273c82de90950fb300a70b790f77c8e45268b
17000719 <17000719@myrp.edu.sg>
2019-05-09 16:48:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/studenttest/Student.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.studenttest;
+
+public class Student {
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
